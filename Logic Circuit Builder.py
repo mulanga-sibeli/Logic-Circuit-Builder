f21c5f0ec753eeb2bafddf2a3c27b6fc24d83738
mulanga-sibeli <s.mulanga@outlook.com>
2022-01-20 18:45:43 +0200

    Added updated code (To-Do: Drawing function)

--- Logic Circuit Builder.py
+++ Logic Circuit Builder.py
@@ -85,6 +85,7 @@ def NOT_(type, inputs):
         else:
             return 1
 
+number_of_clicks = 0
 class Window(Frame):
     name_count = 65
     outputs = []
@@ -96,9 +97,10 @@ class Window(Frame):
     three_ = [[[],[],[],[],[]],[[],[],[],[],[]],[[],[],[],[],[]],[[],[],[],[],[]],[[],[],[],[],[]]]
     four_ = [[[],[],[],[],[]],[[],[],[],[],[]],[[],[],[],[],[]],[[],[],[],[],[]],[[],[],[],[],[]]]
     inputs = []
+    drawn_widgets = []
     input_and_output_buttons = []
     def __init__(self, master=None):
-        Frame.__init__(self,master)
+        Frame.__init__(self, master)
         self.master = master
         self.init_window()
 
@@ -113,8 +115,9 @@ class Window(Frame):
         mainreset_.pack(side=RIGHT, padx = 1, pady = 5)
         creategate_ = Button(mainframe_, text="Create",command=self.create_gate_window_,bg="orange")
         creategate_.pack(side=LEFT, padx=5,pady=5)
-        undo_gate = Button(mainframe_, text="Guide", command=lambda:[self.error_wrapper_("Guide", "Author: Mulanga Sibeli.\n\nLogic-Circuit-Builder Guide:\n\nThe Create Gate button allows you to create a new gate.\n\nYou can specificy details about the gate you would like to create:\n\nRow: The Row your gate should be in. Range: (1-4).\nCol: The Column your gate should be in. Range: (1-4).\n\nType: The type of gate you would like to create.\n\nInputs: The number of inputs your gate should take.\n\nEach gate will be allocated a unique ID. These IDs will be uppercase letters.\n\nAfter creating your gates, you can now use the Link button which will open\na new window allowing you to specify inputs for each gate.\nEach gate will have a corresponding text box allowing you to type out your inputs.\n\nExamples of inputs:\n\nA11 (This means that the gate given this input will take 1 and 1 and gate A's output as input).\n\n01C (This means that the gate given this input will take 0 and 1 and gate C's output as input).\n\nAfter specifying the inputs, use the Done button which will\nnow take you back to the first window to display the resulting logic circuit.\n\nNote that you can remove everything on the screen\nand create a new circuit by using the Reset button.\n\nHAVE FUN!", 600, 600)] ,bg = "orange")
-        undo_gate.pack(side=LEFT, padx=5, pady=5)
+        guide_button = Button(mainframe_, text="Guide", command=lambda:[self.error_wrapper_("Guide", "Author: Mulanga Sibeli.\n\nLogic-Circuit-Builder Guide:\n\nThe Create Gate button allows you to create a new gate.\n\nYou can specificy details about the gate you would like to create:\n\nRow: The Row your gate should be in. Range: (1-4).\nCol: The Column your gate should be in. Range: (1-4).\n\nType: The type of gate you would like to create.\n\nInputs: The number of inputs your gate should take.\n\nEach gate will be allocated a unique ID. These IDs will be uppercase letters.\n\nAfter creating your gates, you can now use the Link button which will open\na new window allowing you to specify inputs for each gate.\nEach gate will have a corresponding text box allowing you to type out your inputs.\n\nExamples of inputs:\n\nA11 (This means that the gate given this input will take 1 and 1 and gate A's output as input).\n\n01C (This means that the gate given this input will take 0 and 1 and gate C's output as input).\n\nAfter specifying the inputs, use the Done button which will\nnow take you back to the first window to display the resulting logic circuit.\n\nNote that you can remove everything on the screen\nand create a new circuit by using the Reset button.\n\nHAVE FUN!", 600, 600)] ,bg = "orange")
+        guide_button.pack(side=LEFT, padx=5, pady=5)
+        
 
     def main_reset(self):
         self.name_count = 65
